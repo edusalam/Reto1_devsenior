ecec0565565940f371aa25456de844278f175b3e
edusalam <eduarfernando.salcedo@gmail.com>
2024-11-21 16:37:22 -0500

    comic actualizado

--- main.py
+++ main.py
@@ -3,38 +3,43 @@
 #visualizacin print consola
 #atributos
 #atributos de un experimento: nombre, fecha, tipo de resultados
+#import datatime
+
 LISTADeExperimentos = [
     "experimento 1", "16/11/2024", "quimica", [5,3,4,5,6,44],
 ]
-import datatime
+
 def agregarExperimento():
-    #funcion permite un nuevo experimento con sus atributos
+    #permite un nuevo experimento con sus atributos
     print("prueba github")
     pass
-#permite eliminar un experimento,    
+
+   
 def eliminarExperimento():
+    #permite eliminar un experimento, 
  pass
+
 def visualizarExperimento():
     #permite visualizar todos los experimentos, requiere el uso de la funcion agregarr experimento dificulatas 1 
     pass
- #calcular estadisticasbasicas, promedio maximos y minimos de un experimento, requiere el uso de funcion agrear experimento prioridad 2
+
+ 
 def calcularEstadistica():
+    #calcular estadisticasbasicas, promedio maximos y minimos de un experimento, requiere el uso de funcion agrear experimento prioridad 2
     pass
 
 def compararExperimento():
     #compara 2 o mas experimentos para determinar los mejores o peores resultados, requiere el uso de calcular estadistica dificula 2
-   
     pass
+
 def generarInforme():
     #generar un informe resumido de los experimentos y sus estadisticas
     pass
-#muestra el menu principal del programa
 
-#prueba borrar###################
 
-#mejorar el menu 1
-#mejorar el informe por que re  quiere el uso de ufnciones y calcular para su uso
 def mostrarMenu():
+    #mejorar el menu 1
+    #mejorar el informe por que re  quiere el uso de ufnciones y calcular para su uso
     print("=========Menu Principal==0=======")
     print("=========Gestion de experimentos=========")
     print("1. agregarexperimento")
@@ -45,9 +50,7 @@ def mostrarMenu():
     print("5.comparar experimentos")
     print("=========informes=========")
     print("6.Generar informe")
-    print("7. salir")
-    
-        
+    print("7. salir")   
     pass
 #funcion principal controla el flujo general del sistema
 def main():
