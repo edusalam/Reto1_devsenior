6e5d7b9b13ecc30ae208df08bde85fa9e5e168d8
edusalam <eduarfernando.salcedo@gmail.com>
2024-11-25 03:46:46 -0500

    creación función comparar experimento

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-#print("hola")        INVESTIGAR LA MAQUETACION
-#estructurar para que haya un buen funcionamiento
-#visualizacin print consola
-#atributos
-#atributos de un experimento: nombre, fecha, tipo de resultados
+
 ListaExperimentos = [
     "experimento 1", "16/11/2024", "quimica", [5,3,4,5,6,44],
 ]
 
-##librerias importadas
 from datetime import datetime
 import statistics
 
 class Experimento:
 
-    ##metodo constructor 
-    def __init__(self, nombreExperimento, fechaDeRealizacion, tipoExperimento, resultadosExperimento):
+    #metodo constructor 
+ def __init__(self, nombreExperimento, fechaDeRealizacion, tipoExperimento, resultadosExperimento):
         self.nombreExperimento = nombreExperimento
         self.fechaDeRealizacion = fechaDeRealizacion
         self.tipoExperimento = tipoExperimento
         self.resultadosExperimento = resultadosExperimento
+    
 
 ## funcion agregar experimento 
 def agregarExperimento(listaExperimentos):
@@ -54,9 +50,12 @@ def eliminarExperimento():
 
 ## funsion para ver todos los experimentos agregados
 def visualizarExperimentos(listaExperimentos):
-    if not listaExperimentos:
-        print("no hay experimentos agregados")
-        return
+    print("Lista de experimentos")
+    for i, experimento in enumerate(listaExperimentos):
+        print(f"{i}.{experimento[0]} - {experimento[1]} - {experimento[2]} - {experimento[3]} ")
+    #if not listaExperimentos:
+     #   print("no hay experimentos agregados")
+      #  return
     
     for i,experimento in enumerate(listaExperimentos, start=1):
         print(f"\nexperimento {i}")
@@ -64,14 +63,26 @@ def visualizarExperimentos(listaExperimentos):
         print(f"fecha limite: {experimento.fechaDeRealizacion.strftime('%d, %m, %Y')}")
         print(f"categoria: {experimento.tipoExperimento}")
         print(f"horas dedicadas: {experimento.resultadosExperimento}")
-
         
 #calcular estadisticasbasicas, promedio maximos y minimos de un experimento, requiere el uso de funcion agrear experimento prioridad 2
 ## analisis de resultados
 def calcularEstadisticas(listaExperimentos):
-    if not listaExperimentos:
-        print("no hay experimentos agregados")
-        return  
+    visualizarExperimentos():
+    print("listado de experimentos")
+    index = int(input("ingrese el numero del experimento: ")) 
+    if(0 <= index < len(listaExperimentos)):
+        resultados = listaExperimentos[index][3]
+        promedio = sum(resultados)/len(resultados)
+        maximo = max(resultados)
+        minimo = (resultados)
+        print(f"estadisticas del experimento {listaExperimentos[index][0]}")
+        print(f"promedio: {promedio}")
+        print(f"maximo: {maximo}")
+        print(f"minimo: {minimo}")
+
+    #if not listaExperimentos:
+     #   print("no hay experimentos agregados")
+      #  return  
     
     for experimento in listaExperimentos:
         promedio = statistics.mean(experimento.resultadosExperimento)
@@ -82,12 +93,27 @@ def calcularEstadisticas(listaExperimentos):
         print(f"puntaje maximo de los resultados{maximo} ")
         print(f"puntaje minimo de los resultados{minimo}")
 
-print("actualizaco 24 noviembre")
-print("prueba uno rama uno")
+def compararExperimento():   
+    visualizarExperimentos()
+    indices = list(map(int, input("ingrese los indices de los equipos que desesa comparar separados  por comas: ").split(","))) 
+                                  
+    resultados_comparacion = []   
+    for index in indices:
+         if(0 <= index < len(listaExperimentos)):
+            promedio = sum(ListaExperimentos[index][3]) / len(ListaExperimentos[index][3])
+            resultados_comparacion.append(promedio)
+    else:
+        print(f"indice {index} invalido")
+    resultados_comparacion.sort(key=lambda x: x[1])
+    print("resultados comparados")
+    for index, promedio in resultados_comparacion:
+        print(f"{index +1}.{ListaExperimentos[index][0]} - {promedio}") 
+        
+                  
+      
 
-
-def compararExperimento():             #xxxxxxxxxxxxxxxxxxxx
     #compara 2 o mas experimentos para determinar los mejores o peores resultados, requiere el uso de calcular estadistica dificula 2
+    #parar comparar requiere de visualizar los experimentos
     pass
 
 #generar un informe resumido de los experimentos y sus estadisticas
